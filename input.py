0605bbc7603035bbbc9bef11e5a271df2d787c50
Ganji-Venkatesh-09 <venkateshganji513@gmail.com>
2026-02-02 17:36:30 +0530

    Here solve variable names and variable type

---
+++ input.py
@@ -0,0 +1,60 @@
+x = 5
+y = "venky"
+print(x)
+print(y)
+
+x = 8
+x = "Income"
+print(x)
+
+x = str(8)
+y = float(4)
+z = int(4)
+print(x)
+print(y)
+print(z)
+
+x = 5
+y = "Heroo"
+print(type(x))
+print(type(y))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
