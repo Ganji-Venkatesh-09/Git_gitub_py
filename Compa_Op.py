b6fa4aacd4eb837984f0a92bf31987b88830b003
Ganji-Venkatesh-09 <venkateshganji513@gmail.com>
2026-02-10 08:01:40 +0530

    Comparision Operators problems on chatgpt till here 15 problems

---
+++ Compa_Op.py
@@ -0,0 +1,126 @@
+#Check whether two numbers are equal.
+num1 = 54
+num2 = 54
+if num1 == num2:
+    print("The Both Numbers Are Equal")
+else:
+    print("The Both Numbers Are Not Equal")
+
+#Check whether two numbers are not equal.
+first_num = 89 
+second_num = 76
+if first_num != second_num:
+    print("The bothe numbers are not equal")
+else:
+    print("Bothe numbers are equal")
+
+#Check whether the first number is greater than the second number.
+x = 89
+y = 76
+if x > y:
+    print("The first number is greater than the second number ")
+else:
+    print("The first number is not greater than the second number")
+
+#Check whether the first number is less than the second number.
+a = 67
+b = 78
+if a < b:
+    print("The first number is less than the second number")
+else:
+    print("The first number is not less than the second number")
+
+#Check whether a number is greater than or equal to another number.
+equal_num = 66
+equal_num2= 46
+if equal_num >= equal_num2:
+    print("The number is greater than or equal to another number")
+else:
+    print("The number is not greater than or equal to another number")
+
+#Check whether a number is less than or equal to another number.
+less_than = 76
+less_than1 = 56
+if less_than <= less_than1:
+    print("The number is less than or equal to another number")
+else:
+    print("The number is not less than or equal to another number ")
+
+#Check whether a person is eligible to vote based on age.
+Person_Age = 18
+if Person_Age >= 18:
+    print("The person is eligible for voting")
+else:
+    print("The person is NOT eligible for voting")
+
+#Check whether a student has passed by comparing marks.
+total_marks = 100
+pass_marks = 40
+student_marks = 75
+if student_marks >= pass_marks :
+    print("The student has passed") 
+else:
+    print ("The student has NOT passed")
+
+#Check whether two strings have the same length.
+string1 = "length"
+string2 = "length"
+if len(string1) == len(string2):
+    print("The two string length is EQUAL")
+else:
+    print("The two strings length is NOT equal")
+
+#Check whether the temperature is above normal.
+normal_temperature = 38
+curent_temperature = 43
+if curent_temperature > normal_temperature:
+    print("The temperature is above ")
+else:
+    print("Temperatur is normal")
+
+#Check whether a number lies below a given limit.
+number = 67
+limit = 100
+if number < limit:
+    print("The number lies below the given limt")
+else:
+    print("The number is does not lies below a given limit")
+
+#Check whether two exam scores are different.
+first_exam_score = 85
+second_exam_score = 92
+if first_exam_score != second_exam_score:
+    print("The two exams scores are diffrent")
+else:
+    print("The two exam scores are NOT diffrent")
+
+#Check whether a number is positive.
+number = 67
+if number > 0:
+    print("The number is positive")
+else:
+    print("The number is negative ")
+
+##Check whether a number is positive, negative, or zero.
+number123 =0
+if number123 > 0:
+    print("The number is positive")
+elif number123 < 0:
+    print("The number is negative")
+else:
+    print("The number is zero")
+
+#Check whether the first value is not smaller than the second value.
+first_value = 89
+second_value = 78
+if first_value >= second_value:
+    print("first value is not smaller than the second value")
+else:
+    print("first value is smaller than the second value")
+
+    
+
+
+
+
+
